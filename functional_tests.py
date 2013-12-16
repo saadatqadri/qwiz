ebcf6a9bc47fb0feee84e79259af6491fd2f3e91
redmosquito <saadat.qadri@gmail.com>
2013-12-16 11:51:07 -0500

    Functional test now checks we can input a quiz question

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 #functional_tests.py
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
 import unittest
 
 
@@ -17,19 +18,39 @@ class NewVisitorTest(unittest.TestCase):
 
 		#John's heard about a cool new online learning management tool. He goes to check out the homepage
 		self.browser.get('http://localhost:8000')
+		
 		# He notices the page title and header mention class quizzes
 		self.assertIn('Qwiz', self.browser.title)
-		self.fail('Finish the test')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Qwiz', header_text)
 
 		# He is invited to create a quiz right away. 
 
+		inputbox = self.browser.find_element_by_id('id_new_question')
+		self.assertEqual(
+				inputbox.get_attribute('placeholder'),
+				'Enter a quiz question'
+		)
 		# He types in the first question into a text box: "Have you taken a course on Python before?"
+
+		inputbox.send_keys('Have you taken a course on Python before?')
 
 		# When he hits Enter, the page updates, and now the page lists 
 		# '1. Have you taken a course on Python before?' as a question in the quiz
 
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+
+		self.assertTrue(
+			any(row.text == '1: Have you taken a course on Python before?' for row in rows)
+		)
+
 		# There is still a text box inviting him to add another quiz question. He enters
 		# "Do you know what the term object-oriented programming means?"
+
+		self.fail('Finish the test!')
 
 		# The page updates again, and shows both questions in the quiz list
 
